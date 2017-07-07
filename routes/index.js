8e3b6d546621e0af1823f228d02e368d74b69fc4
Davy'd <pdaswift3009@gmail.com>
2017-07-07 18:34:54 +0100

    add user model

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+    router = express.Router(),
+    passport = require('passport'),
+    User = require('../models/user');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
